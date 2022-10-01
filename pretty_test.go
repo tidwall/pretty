9090695766b652478676cc3e55bc3187056b1ff0
tidwall <joshbaker77@gmail.com>
2022-10-01 13:21:24 -0700

    Update terminal colors
    Minor update to make the colors a bit more visible on light
    background screen.

--- pretty_test.go
+++ pretty_test.go
@@ -373,15 +373,15 @@ func TestColor(t *testing.T) {
 "arr":["1","2",1,2,true,false,null],
 "obj":{"key1":null,"ar`+"\x1B[36m"+`Cyanr2":[1,2,3,"123","456"]}}
 	`)), nil)
-	if string(res) != `{
-  [94m"hello"[0m: [92m"world"[0m,
-  [94m"what"[0m: [93m123[0m,
-  [94m"arr"[0m: [[92m"1"[0m, [92m"2"[0m, [93m1[0m, [93m2[0m, [96mtrue[0m, [96mfalse[0m, [91mnull[0m],
-  [94m"obj"[0m: {
-    [94m"key1"[0m: [91mnull[0m,
-    [94m"ar\u001b[36mCyanr2"[0m: [[93m1[0m, [93m2[0m, [93m3[0m, [92m"123"[0m, [92m"456"[0m]
-  }
-}
+	if string(res) != `[1m{[0m
+  [1m[94m"hello"[0m[1m:[0m [32m"world"[0m[1m,[0m
+  [1m[94m"what"[0m[1m:[0m [33m123[0m[1m,[0m
+  [1m[94m"arr"[0m[1m:[0m [1m[[0m[32m"1"[0m, [32m"2"[0m, [33m1[0m, [33m2[0m, [36mtrue[0m, [36mfalse[0m, [2mnull[0m[1m][0m[1m,[0m
+  [1m[94m"obj"[0m[1m:[0m [1m{[0m
+    [1m[94m"key1"[0m[1m:[0m [2mnull[0m[1m,[0m
+    [1m[94m"ar\u001b[36mCyanr2"[0m[1m:[0m [1m[[0m[33m1[0m, [33m2[0m, [33m3[0m, [32m"123"[0m, [32m"456"[0m[1m][0m
+  [1m}[0m
+[1m}[0m
 ` {
 		t.Fatal("invalid output")
 	}
